9fa54c87f77192d30f504d585a6aaf3d5702d646
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-23 18:55:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,5 @@ const setThisWithCall = function(fn, thisValue, arg){
 const setThisWithApply = function(fn,thisValue, args){
   return fn.apply(thisValue,args);
 }
+
+const returnNewFunctionOf = function(functionToBeCopied, thisValue)
